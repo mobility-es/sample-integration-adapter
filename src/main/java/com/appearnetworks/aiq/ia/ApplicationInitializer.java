9e73370dbe28c8db1439e51c5e9063989ae5b7d1
mal <mal@appearnetworks.com>
2013-05-31 11:27:16 +0200

    [#45303429] randomly select damage descriptions.

--- src/main/java/com/appearnetworks/aiq/ia/ApplicationInitializer.java
+++ src/main/java/com/appearnetworks/aiq/ia/ApplicationInitializer.java
@@ -9,8 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 
 /**
@@ -36,16 +35,24 @@ public class ApplicationInitializer implements InitializingBean {
 
     private void createTrainDamages() {
         List<TrainDO> trainDOs = trainDao.getAll();
+        List<String> trainDamageDescriptions = getTrainDamageDescriptions();
+
+        //pick train damage descriptions randomly for each created train damage report from the list of 20 damage descriptions
+        Random random = new Random();
+        int min = 0;
+        int max = 19;
 
         for (TrainDO trainDO : trainDOs) {
-            TrainDamageReportDO trainDamageReportDO = new TrainDamageReportDO();
+            // create two damage reports per train
+            int range = max - min + 1;
+            int index = random.nextInt(range) + min;
 
-            trainDamageReportDO.setCreationDateTime(new Date());
-            trainDamageReportDO.setDescription("Part number 9507 maintenance.");
-            trainDamageReportDO.setReportedBy("Alex Schulman");
-            trainDamageReportDO.setTrain(trainDO);
+            String description = trainDamageDescriptions.get(index);
+            trainDamageDao.create(createTrainDamageDo(trainDO, description));
 
-            trainDamageDao.create(trainDamageReportDO);
+            index = random.nextInt(range) + min;
+            description = trainDamageDescriptions.get(index);
+            trainDamageDao.create(createTrainDamageDo(trainDO, description));
         }
     }
 
@@ -54,13 +61,49 @@ public class ApplicationInitializer implements InitializingBean {
         trainDao.create(new TrainDO(9001L));
         trainDao.create(new TrainDO(9002L));
         trainDao.create(new TrainDO(9003L));
+        trainDao.create(new TrainDO(9004L));
         trainDao.create(new TrainDO(9005L));
         trainDao.create(new TrainDO(9006L));
         trainDao.create(new TrainDO(9007L));
-        trainDao.create(new TrainDO(9008L));
-        trainDao.create(new TrainDO(9010L));
-        trainDao.create(new TrainDO(9011L));
-        trainDao.create(new TrainDO(9012L));
+    }
+
+    private TrainDamageReportDO createTrainDamageDo(TrainDO trainDO, String description){
+        TrainDamageReportDO trainDamageReportDO = new TrainDamageReportDO();
+
+        trainDamageReportDO.setCreationDateTime(new Date());
+        trainDamageReportDO.setDescription(description);
+        trainDamageReportDO.setReportedBy("Alex Schulman");
+        trainDamageReportDO.setTrain(trainDO);
+
+        return trainDamageReportDO;
+    }
+
+    private List<String> getTrainDamageDescriptions(){
+        String[] damageDescriptions = {
+                "Paper towels are missing in cabinet",
+                "A3000 OMR faulty. A3004 faulty. Sometimes A3040 OMR faulty in section B, losing charge",
+                "30C + in cabin, sometimes cold. No error reported",
+                "Compressed air system shows high oil temp. Left it to cool down. Hopefully fine now",
+                "Vehicle information systems displays error B8480",
+                "Door number 4 is malfunctioning. Out of order",
+                "Poor ventilation in passenger cabin. No cold air available",
+                "Headlight beam, left hand side, not functioning",
+                "Monitor B, poor picture quality",
+                "The vehicle needs exterior cleaning. I cannot see anything through rear view mirrors/ cameras",
+                "Toilet malfunction. Cannot flush.",
+                "Brake system computer communications error. Codes 8472, 8476",
+                "DMB vehicle traction engine fan 2: Sending code: C 4076",
+                "Toilet excrement tank more than 50% full!",
+                "Graffiti on car exterior, section DMA, DMB",
+                "Passenger cabin window does not open. Handle is broken",
+                "Water closet sink is blocked",
+                "B8411 primary brake computer shows comm error",
+                "Out of washer fluid for windscreen wipers",
+                "Information system screens malfunction. Blank screens",
+                "First aid kit is not sealed"
+        };
+
+        return Arrays.asList(damageDescriptions);
     }
 
 
